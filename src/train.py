ae8f706819f1de2c626f94465dbfc6d99c591f0d
hsthe29 <The.HS200614@sis.hust.edu.vn>
2023-05-23 21:26:25 +0700

    add config.yml file

--- src/train.py
+++ src/train.py
@@ -3,21 +3,25 @@ from src.utils import *
 from src.model.base_model import *
 from src.losses import *
 from src.metrics import *
+import yaml
 
 if __name__ == "__main__":
+    with open("config.yml") as f:
+        config = yaml.load(f, Loader=yaml.SafeLoader)
 
-    label_name, data = get_data_from_csv("path_to_csv")
-    image_paths, labels = extract_path_and_label("image_dir", data)
+    label_name, data = get_data_from_csv(config['csv_file'])
+    image_dir = config['image_dir']
+    image_paths, labels = extract_path_and_label(image_dir, data)
 
     NUM_CLASSES = len(label_name)
 
-    train_raw, val_raw = get_dataset(image_paths, labels, ratio=8.5)
+    train_raw, val_raw = get_dataset(image_paths, labels, ratio=config['train_test_ratio'])
     train_ds = train_raw.map(convert_path, num_parallel_calls=tf.data.AUTOTUNE)
     val_ds = val_raw.map(convert_path, num_parallel_calls=tf.data.AUTOTUNE)
 
     pretrained = load_pretrained()
 
-    hidden_units = 1024
+    hidden_units = config['hidden_units']
 
     model = BaseMultiLabelCNN(pretrained, hidden_units, NUM_CLASSES)
 
@@ -25,12 +29,13 @@ if __name__ == "__main__":
                   loss=cross_entropy_loss_with_logits, metrics=[accuracy])
 
     print("Call model on batch of data for building graph:")
-    model.evaluate(val_ds, steps=10, return_dict=True)
+    eval_result = model.evaluate(val_ds, steps=5, return_dict=True)
+    print(eval_result)
 
     print("Training: ")
     history = model.fit(
         train_ds,
-        epochs=50,
+        epochs=config['epochs'],
         validation_data=val_ds,
         callbacks=[tf.keras.callbacks.EarlyStopping(patience=5)])
 
@@ -39,4 +44,3 @@ if __name__ == "__main__":
     plot_history(history.history)
 
     # Evaluate model
-
