4b02003a69abde8bec2eedc489d91b5a4aedff27
Kiet Tuan <101494254+ktcshust@users.noreply.github.com>
2023-06-17 12:04:11 +0700

    Update vitl16.py

--- src/model/vitl16.py
+++ src/model/vitl16.py
@@ -3,6 +3,44 @@ from tensorflow import keras
 from src.utils import load_pretrained
 
 
+class PositionalEncoding(keras.layers.Layer):
+    def __init__(self):
+        super(PositionalEncoding, self).__init__()
+
+    def call(self, inputs):
+        batch_size, seq_length, d_model = tf.shape(inputs)
+        position_indices = tf.range(seq_length)[:, tf.newaxis]
+        div_term = tf.pow(10000.0, 2 * tf.range(d_model // 2, dtype=tf.float32) / d_model)
+        positional_encoding = tf.concat([tf.sin(position_indices / div_term), tf.cos(position_indices / div_term)], axis=-1)
+        positional_encoding = tf.reshape(positional_encoding, [1, seq_length, d_model])
+
+        return inputs + positional_encoding
+
+
+class TransformerLayer(keras.layers.Layer):
+    def __init__(self, d_model, num_heads, mlp_dim, dropout_rate):
+        super(TransformerLayer, self).__init__()
+        self.multihead_attention = keras.layers.MultiHeadAttention(num_heads=num_heads, key_dim=d_model // num_heads)
+        self.dropout1 = keras.layers.Dropout(dropout_rate)
+        self.layer_norm1 = keras.layers.LayerNormalization()
+        self.mlp = keras.Sequential([
+            keras.layers.Dense(mlp_dim, activation='relu'),
+            keras.layers.Dense(d_model),
+        ])
+        self.dropout2 = keras.layers.Dropout(dropout_rate)
+        self.layer_norm2 = keras.layers.LayerNormalization()
+
+    def call(self, inputs):
+        attention_output = self.multihead_attention(inputs, inputs)
+        attention_output = self.dropout1(attention_output)
+        attention_output = self.layer_norm1(inputs + attention_output)
+        mlp_output = self.mlp(attention_output)
+        mlp_output = self.dropout2(mlp_output)
+        output = self.layer_norm2(attention_output + mlp_output)
+
+        return output
+
+
 class ViTL16(keras.Model):
     def __init__(self, hidden_units, num_classes, patch_size=16, num_layers=16, d_model=256, num_heads=8, mlp_dim=512, dropout_rate=0.1):
         super(ViTL16, self).__init__()
@@ -20,8 +58,7 @@ class ViTL16(keras.Model):
     def call(self, batch_input):
         feature_maps = self.pretrained(batch_input)
         patches = self.patch_embedding(feature_maps)
-        batch_size, num_patches, _, d_model = patches.shape
-        patches = tf.reshape(patches, [batch_size, num_patches, d_model])
+        patches = tf.squeeze(patches, axis=1)
         patches = self.positional_encoding(patches)
         for layer in self.transformer_layers:
             patches = layer(patches)
@@ -37,7 +74,8 @@ class ViTL16(keras.Model):
             batch_input = batch_input[tf.newaxis]
 
         logits = self(batch_input)
-        pred = logits > 0.0
+        pred = tf.math.argmax(logits, axis=-1)
         if as_int:
             return tf.cast(pred, dtype=tf.int32)
         return tf.cast(pred, dtype=tf.float32)
+
